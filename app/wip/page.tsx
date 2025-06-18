dc9a71e0aabb5ff2028d44dea52f0e136f3a4ed4
Sudip Biswas <sudipbiswas2142@gmail.com>
2025-06-18 19:11:49 +0530

    add wip page

---
+++ app/wip/page.tsx
@@ -0,0 +1,9 @@
+export default function WIPPage() {
+  return (
+    <main className="w-full min-h-dvh grid place-items-center p-6">
+      <h1 className="font-semibold text-2xl sm:text-3xl tracking-tight text-center">
+        We&apos;re working on something great.
+      </h1>
+    </main>
+  );
+}
